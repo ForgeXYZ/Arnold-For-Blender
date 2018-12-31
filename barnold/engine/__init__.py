9003d72418905732252afde63b6137a553067762
Tyler Furby <tyler@tylerfurby.com>
2018-12-30 18:54:02 -0700

    Fix crashes once and for all

--- barnold/engine/__init__.py
+++ barnold/engine/__init__.py
@@ -548,28 +548,18 @@ def _export(data, depsgraph, camera, xres, yres, session=None):
     @contextmanager
     def _Mesh(ob):
         pc = time.perf_counter()
-        mesh = ob.to_mesh(depsgraph, apply_modifiers=True, calc_undeformed=True)
-        if mesh is not None:
-            if mesh.users == 0:
-                try:
-                    mesh.calc_normals_split()
-                    arnold.AiMsgDebug(b"    mesh (%f)", ctypes.c_double(time.perf_counter() - pc))
-                    mesh.user_clear()
-                    can_continue = True
-                    yield mesh
-                except:
-                    can_continue = False
-                if can_continue:
-                    try:
-                        mesh = bpy.data.meshes.get(ob.name)
-                        bpy.data.meshes.remove(mesh)
-                        result = True
-                    except:
-                        result = False
-                else:
-                    yield None
-        else:
-            yield None
+        mesh = None
+        try:
+            mesh = ob.to_mesh(depsgraph=depsgraph, apply_modifiers=True, calc_undeformed=False)
+
+            if mesh:
+                mesh.calc_normals_split()
+                arnold.AiMsgDebug(b"    mesh (%f)", ctypes.c_double(time.perf_counter() - pc))
+
+            yield mesh
+        finally:
+            if mesh:
+                bpy.data.meshes.remove(mesh, do_unlink=False)
 
     _Name = _CleanNames("O", itertools.count())
 
